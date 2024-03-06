e8c8bd81a8c5f600a2de5c3f26445e0119cce338
tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
2024-03-06 14:24:53 +0530

    KB-2925 | New API  for the OTP generation for email/phone verification (#62)
    * KB-2925 | New API  for the OTP generation for email/phone verification
    
    1. OTP access token enhancements
    
    * KB-2925 | New API for the OTP generation for email/phone verification
    
    1. Secret Key added to external resource properties file.
    
    * KB-2925 | New API for the OTP generation for email/phone verification
    
    1. Secret key removed
    
    * KB-2925 | New API for the OTP generation for email/phone verification
    
    1. Otp timeout is configured to 5mins.
    
    * KB-2925 | New API for the OTP generation for email/phone verification
    
    1. Added the column names to the casssandraTablecolumn.properties.
    
    * KB-2925 | New API for the OTP generation for email/phone verification
    
    1. Removed the comment line.

--- core/platform-common/src/main/java/org/sunbird/keys/JsonKey.java
+++ core/platform-common/src/main/java/org/sunbird/keys/JsonKey.java
@@ -649,5 +649,7 @@ public final class JsonKey {
   public static final String CONTEXT_TYPE = "contextType";
   public static final String CONTEXT_ATTRIBUTES = "contextAttributes";
   public static final String CONTEXT_TOKEN = "contextToken";
+  public static final String OTP_VALIDATION_SECRET_KEY = "otpValidationSecretKey";
+  public static final String OTP_EXPIRATION_TIME_TOKEN = "otpExpirationTimeToken";
   private JsonKey() {}
 }
